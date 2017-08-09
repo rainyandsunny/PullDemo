f9f16b480e19b99a1b19689b809f783f3b35643b
yanghaipeng <haipeng.yang@yuntu-inc.com>
2017-08-09 16:29:11 +0800

    clean some unuse code

--- PullDemo/app/src/main/java/com/yuntu/demo/pulldemo/MainActivity.java
+++ PullDemo/app/src/main/java/com/yuntu/demo/pulldemo/MainActivity.java
@@ -2,8 +2,15 @@ package com.yuntu.demo.pulldemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+
+
+    private ListView mDataListView;
+    private View mHeaderView;
+    private View mFootView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
